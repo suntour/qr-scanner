adb7afa37505732d92261070797230a30c6636a8
suntour <37424999+suntour@users.noreply.github.com>
2021-08-28 23:05:14 -0400

    made it easier to tell if successfully scanned

--- scanner.py
+++ scanner.py
@@ -18,19 +18,22 @@ def scan():
             if points is not None:
                     point_len = len(points)
 
-                    pts = np.array(points, np.int32)
-                    pts = pts.reshape(-1,1,2)
-                    cv2.polylines(image, [pts], True, (255,0,255), 5)
-
             hashedCode = hash(decodedText)
             if(not hashedCode in codes):
                 codes[hashedCode] = decodedText
                 print('Adding ' + decodedText + ' to list')
 
+                pts = np.array(points, np.int32)
+                pts = pts.reshape(-1,1,2)
+                cv2.polylines(image, [pts], True, (255,0,255), 5)
+            else:
+                    pts = np.array(points, np.int32)
+                    pts = pts.reshape(-1,1,2)
+                    cv2.polylines(image, [pts], True, (0,255,0), 5)
+
         cv2.imshow("QR Scanner", image)
         k = cv2.waitKey(1)
         if k == 27 or k == ord('q'):
-            print(codes)
             break
 
     camera.release()
